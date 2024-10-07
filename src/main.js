41f10f31453ec5389fac6e0e8311498a12ce67e2
Praba Ponnambalam <praba@codechilli.lk>
2024-10-07 14:05:20 +0530

    🎨 increase confetti

--- src/main.js
+++ src/main.js
@@ -171,15 +171,44 @@ const showWinner = () => {
 
 	winnerDisplay.classList.remove('hidden');
 
-	confetti({
-		particleCount: 800,
-		spread: 180,
-		origin: { y: 0.3 },
-	});
+	fireExtendedConfetti();
 
 	setTimeout(() => {
 		drawAgainBtn.classList.remove('hidden', 'opacity-0');
 	}, 3000);
+};
+
+const fireExtendedConfetti = () => {
+	const duration = 8 * 1000; // 8 seconds
+	const animationEnd = Date.now() + duration;
+	const defaults = { startVelocity: 30, spread: 360, ticks: 120, zIndex: 0 };
+
+	function randomInRange(min, max) {
+		return Math.random() * (max - min) + min;
+	}
+
+	const interval = setInterval(function () {
+		const timeLeft = animationEnd - Date.now();
+
+		if (timeLeft <= 0) {
+			return clearInterval(interval);
+		}
+
+		const particleCount = 100 * (timeLeft / duration);
+
+		confetti(
+			Object.assign({}, defaults, {
+				particleCount,
+				origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+			})
+		);
+		confetti(
+			Object.assign({}, defaults, {
+				particleCount,
+				origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+			})
+		);
+	}, 250);
 };
 
 const resetToHome = () => {
